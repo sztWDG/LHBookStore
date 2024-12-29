6ccb783e1e018ac177f1e0c95e1e58788cdedd50
lionhead <657116847@qq.com>
2024-12-29 17:53:21 +0800

    Init

---
+++ src/main/java/com/example/LhBookStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LhBookStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LhBookStoreApplication.class, args);
+    }
+
+}
